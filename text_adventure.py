8c863bdcee38ab13987865749fb8ef0cfff58bb1
dragon-ball6 <dominicchanzy@gmail.com>
2023-02-19 15:43:01 +0000

    modified:   text_adventure.py

--- text_adventure.py
+++ text_adventure.py
@@ -5,7 +5,7 @@ def choices(*arg):
     print("What will you do?")
     for _i, i in enumerate(arg):
         print(f"{_i}. {i}")
-    print("\n")
+    return int(input(""))
 
 print("You are Mario. You have arrived in the castle, years after Princess Peach's funeral, which you could not attend.")
 print(f"It {Style.BRIGHT}has{Style.RESET_ALL} been so long since you last stepped foot in the castle... But you {Style.BRIGHT}must do it...")
